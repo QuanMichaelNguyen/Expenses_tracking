f97ca4745cb420273d0f2f6186c4ae93119d228b
QuanMichaelNguyen <144957908+QuanMichaelNguyen@users.noreply.github.com>
2024-01-05 23:06:15 -0500

    Second commit

--- tracking_app.py
+++ tracking_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 def index():
     # return render_template('index.html')
-    return "Hello world"
+    return "This is expense tracking app"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
